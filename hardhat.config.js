023aba30a292fb7f1b901a44bb77717eb1a56284
nayanchoudhary31 <nayanscsit09@gmail.com>
2023-07-13 11:25:26 +0530

    Added Coincapmarket API Key

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,41 +1,40 @@
-require("@nomicfoundation/hardhat-toolbox");
-require('@nomicfoundation/hardhat-verify');
-require('hardhat-gas-reporter')
-require('./tasks/blocknumber');
-require("solidity-coverage");
-require('dotenv').config();
+require("@nomicfoundation/hardhat-toolbox")
+require("@nomicfoundation/hardhat-verify")
+require("hardhat-gas-reporter")
+require("./tasks/blocknumber")
+require("solidity-coverage")
+require("dotenv").config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.7",
-  defaultNetwork:"hardhat",
-  networks:{
-
-    localhost:{
-      url:"http://127.0.0.1:8545/",
-      chainId:31337
+    solidity: "0.8.7",
+    defaultNetwork: "hardhat",
+    networks: {
+        localhost: {
+            url: "http://127.0.0.1:8545/",
+            chainId: 31337,
+        },
+        mumbai: {
+            url: process.env.MUMBAI_MATIC_RPC_URL,
+            accounts: [process.env.PRIVATE_KEY],
+            chainId: 80001,
+        },
+        sepolia: {
+            url: process.env.SEPOLIA_RPC_URL,
+            accounts: [process.env.PRIVATE_KEY],
+            chainId: 11155111,
+        },
     },
-    mumbai:{
-      url:process.env.MUMBAI_MATIC_RPC_URL,
-      accounts:[process.env.PRIVATE_KEY],
-      chainId:80001
+    etherscan: {
+        apiKey: {
+            sepolia: process.env.ETHERSCAN_API_KEY,
+            polygonMumbai: process.env.POLYGON_API_KEY,
+        },
     },
-    sepolia:{
-      url:process.env.SEPOLIA_RPC_URL,
-      accounts:[process.env.PRIVATE_KEY],
-      chainId:11155111
-    }
-  },
-  etherscan:{
-    apiKey:{
-      sepolia:process.env.ETHERSCAN_API_KEY,
-      polygonMumbai:process.env.POLYGON_API_KEY
-    }
-  },
-  gasReporter:{
-    enabled:false,
-    outputFile:"gas-report.txt",
-    // currency:"USD",
-    // coinmarketcap:process.env.COINCAPMARKET_API_KEY
-  }
-};
+    gasReporter: {
+        enabled: true,
+        outputFile: "gas-report.txt",
+        currency: "USD",
+        coinmarketcap: process.env.COINCAPMARKET_API_KEY,
+    },
+}
